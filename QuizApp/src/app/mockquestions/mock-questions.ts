f40b8576cf0d4abef3a3abcd3a9752e837ba50ae
noemipi <n.pisano@gmail.com>
2019-08-02 12:32:59 +0200

    Json Class Definition

---
+++ QuizApp/src/app/mockquestions/mock-questions.ts
@@ -0,0 +1,11 @@
+import { Question } from './question';
+
+export const QUESTIONS: Question [] = [
+  {
+    id: 1,
+    tag: [],
+    text: "",
+    answertype: "",
+    answers:"",
+  }
+];
---
+++ QuizApp/src/app/mockquestions/question.ts
@@ -0,0 +1,13 @@
+export class Question {
+  id: number;
+  tag: string [];
+  text: string;
+  answertype: string;
+  answers: Answer[];
+}
+
+export class Answer {
+  value: string;
+  text: string;
+  correct: boolean;
+}
