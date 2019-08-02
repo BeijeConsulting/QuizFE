2fd240f8c678b9577bf32565a3bbb72e22dcb5b5
ThetaBet <thom.bella@icloud.com>
2019-08-02 15:53:20 +0200

    restart

---
+++ QuizApp/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+
+
+const routes: Routes = [
+  {path: '', component: LoginComponent },
+  {path: 'user', component: UserComponent},
+  {path: 'admin', component: AdminComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
