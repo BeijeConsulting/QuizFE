2f2048332d5c048b239ab430c27e54c66a425b00
BlancoTarrendi <tentimauro@gmail.com>
2019-08-07 17:51:16 +0200

    fix stuff

--- QuizApp/src/app/questions.service.ts
+++ QuizApp/src/app/questions.service.ts
@@ -11,22 +11,12 @@ export class QuestionsService {
   questionsUrl = 'api/questions';
   tagsUrl = 'api/tags';
 
-  tagsUrl = 'api/tags'
-
   constructor(private http: HttpClient) { }
 
   getQuestions(): Observable<Question[]> {
     return this.http.get<Question[]>(this.questionsUrl);
   }
 
-<<<<<<< HEAD
-  getTags(): Observable<string[]> {
-    return this.http.get<string[]>(this.tagsUrl);
-  }
-  searchQuestions(tagValue: string[]): Observable<Question[]>{
-    console.log(this.questionsUrl + '?tag=' + tagValue.join('&tag='))
-    return this.http.get<Question[]>(this.questionsUrl + '?tag=' + tagValue.join('&tag='))
-=======
   getQuestion(id: number): Observable<Question> {
     const url = `${this.questionsUrl}/${id}`;
     return this.http.get<Question>(url);
@@ -34,13 +24,8 @@ export class QuestionsService {
   getTags(): Observable<string[]> {
     return this.http.get<string[]>(this.tagsUrl)
   }
-  searchTag(term: string) {
-/*    if (!term.trim()) {
-      // if not search term, return empty hero array.
-      return of([]);
-    }
-    return this.http.get<string[]>(`${this.tagsUrl}?tag=${term}`)*/
-
->>>>>>> master
+  searchQuestions(tagValue: string[]): Observable<Question[]>{
+    console.log(this.questionsUrl + '?tag=' + tagValue.join('&tag='))
+    return this.http.get<Question[]>(this.questionsUrl + '?tag=' + tagValue.join('&tag='))
   }
 }
